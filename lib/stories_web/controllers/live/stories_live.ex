c8b46215053b67397fbafc731877935561099fd1
zvlasic <zlayo1@gmail.com>
2024-05-15 12:23:18 +0200

    Improve prompt

--- lib/stories_web/controllers/live/stories_live.ex
+++ lib/stories_web/controllers/live/stories_live.ex
@@ -64,17 +64,17 @@ defmodule StoriesWeb.StoriesLive do
     story_prompt =
       """
       Please, write a #{story_length} short story for a #{kid}, so please no big words.
-      The tale should feat an unconventional hero named #{hero}.
+      The tale should feature an unconventional hero - #{hero}.
       The #{hero} is not a typical hero but has unique attributes and strengths. Describe a single situation where these peculiarities assist #{hero} in surprising ways.
-      The journey should feature a villain called #{villain}. This villain's backstory needs to be complex and interesting. Describe how #{villain}'s past experiences, especially those connected with #{hero}, led them down the path of villainy.
+      The journey should feature a villain - #{villain}. This villain's backstory needs to be complex and interesting. Describe how #{villain} past experiences, especially those connected with #{hero}, led them down the path of villainy.
       The theme of the story is #{theme}. Explore how this theme is relevant to both #{hero} and #{villain}.
       Aim for a #{story_tone} tone. Keep in mind that the story should stimulate children's imagination. It can be whimsical, playful, and filled with unexpected twists.
       Introduce at least one amusing joke or funny situation that's suitable for a child's understanding and sense of humor.
       Remember to divide the story into different sections or 'episodes' represented by paragraphs, each with its unique mini-adventure or problem-solving sequence.
       A final super battle at the end of the story is a must. The battle should be exciting and filled with action, but not too violent or scary.
       There is no need for a moral or lesson at the end of the story.
-      I just want #{kid} to have fun and be entertained, and for #{hero} to beat #{villain}.
-      Please wrap story paragraphs in hrml paragraph tags.
+      I just want the child to have fun and be entertained, and for #{hero} to beat #{villain}.
+      Please wrap story paragraphs in html paragraph tags.
       """
 
     image_prompt = """
